de7a46c8f80174269086b87e60dfbcb63c22cd4d
MakkaPakka <1643496341@qq.com>
2021-01-26 19:18:03 +0800

    首页请求数据

---
+++ src/views/home/Home.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="home" class="home-nav">
+		<nav-bar>
+			<div slot="center">购物街</div>
+		</nav-bar>
+	</div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home';
+
+export default {
+	name: 'Home',
+	components: {
+		NavBar
+	},
+	data() {
+		return {
+			// result: null
+			banners: [],
+			recommends: []
+		}
+	},
+	created() {
+		// 1.请求多个数据
+		getHomeMultidata().then((res) => {
+			// console.log(res);
+			// this.result = res;
+			this.banners = res.data.banner.list;
+			this.recommends = res.data.recommend.list;
+		}).catch((err) => {
+			
+		});
+	}
+}
+</script>
+
+<style>
+.home-nav {
+	background-color: var(--color-tint);
+	color: white;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { useCssModule } = require("vue");
+
+module.exports = {
+	configureWebpack: {
+		resolve: {
+			// extensions: [], 配置后缀名 已自动配置
+			alias: {
+				'assets': '@/assets',
+				'common': '@/common',
+				'network': '@/network',
+				'components': '@/components',
+				'views': '@/views'
+			}
+		}
+	}
+}
